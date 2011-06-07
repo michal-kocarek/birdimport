a65b62d56a62b8f7830520e2b6d7f43b25a61048
Michal Kočárek <from.github.com@brainbox.cz>
2011-06-07 19:43:00 +0200

    - Remove unnecessary comments

--- chrome/content/importDialogOverlay.js
+++ chrome/content/importDialogOverlay.js
@@ -29,43 +29,12 @@
  * @author Michal Kočárek michal.kocarek@brainbox.cz
  * @since 2011.06.04
  */
-
-const Cc = Components.classes;
-const Ci = Components.interfaces;
-
-// importDialog.xul: http://mxr.mozilla.org/mozilla/source/mailnews/import/resources/content/importDialog.xul
-// importDialog.js: http://mxr.mozilla.org/mozilla/source/mailnews/import/resources/content/importDialog.js
-
-/*
-
-var importService = Components.classes["@mozilla.org/import/import-service;1"].getService()
-
-importService = importService.QueryInterface(Components.interfaces.nsIImportService)
-
-var categoryManager = Components.classes["@mozilla.org/categorymanager;1"].getService();
-categoryManager = categoryManager.QueryInterface(Components.interfaces.nsICategoryManager);
-
-const CATEGORY_MAILNEWS_IMPORT = 'mailnewsimport';
-
-// nsISimpleEnumerator
-var en = categoryManager.enumerateCategory(CATEGORY_MAILNEWS_IMPORT);
-var ar = [];
-while(en.hasMoreElements()) {
-	var prop = en.getNext().QueryInterface(Components.interfaces.nsISupportsCString); // class id
-	var cid = prop.data; // string
-	var im = Components.classesByID[cid].createInstance(Components.interfaces.nsIImportModule);
-	print(''+cid+' - '+prop.data+' ** '+im.name+': '+im.description+'; supports: '+im.supports);
-}
-//print(ar);
-
-// categoryManager.addCategoryEntry("mailnewsimport", "{1faf86b2-90b6-49ee-80ba-dd33612c1ba3}", "mail", false, true);
-
-
-
-*/
-
+	
 (function() {
 	
+	const Cc = Components.classes;
+	const Ci = Components.interfaces;
+
 	/** @type Components.interfaces.nsICategoryManager */
 	var categoryManager = Cc["@mozilla.org/categorymanager;1"].getService().QueryInterface(Ci.nsICategoryManager);
 	
@@ -76,5 +45,3 @@ while(en.hasMoreElements()) {
 	categoryManager.addCategoryEntry('mailnewsimport', Cc['@mozilla.org/import/import-thebat;1'].number, tbImportModule.supports, false, true);
 	
 })();
-// Handling out data in XPCOM components:
-// http://www.synovel.com/content/handling-out-parameters-javascript-xpcom-components
